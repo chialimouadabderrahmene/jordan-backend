d1579006874801ce236d93f26a5aa3c80f70121c
chialimouad <chialimouaduae@gmail.com>
2026-03-07 15:18:53 +0100

    Revert hardcoded pooler connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -11,17 +11,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
                 type: 'postgres',
                 ...(configService.get<string>('database.url')
                     ? { url: configService.get<string>('database.url') }
-                    : configService.get<string>('database.host')?.includes('db.hjojxhcuokbflvemztji.supabase.co')
-                        ? {
-                            url: `postgresql://${configService.get<string>('database.username')}.hjojxhcuokbflvemztji:${configService.get<string>('database.password')}@aws-0-eu-central-1.pooler.supabase.com:6543/${configService.get<string>('database.name')}?pgbouncer=true`,
-                        }
-                        : {
-                            host: configService.get<string>('database.host'),
-                            port: configService.get<number>('database.port'),
-                            username: configService.get<string>('database.username'),
-                            password: configService.get<string>('database.password'),
-                            database: configService.get<string>('database.name'),
-                        }),
+                    : {
+                        host: configService.get<string>('database.host'),
+                        port: configService.get<number>('database.port'),
+                        username: configService.get<string>('database.username'),
+                        password: configService.get<string>('database.password'),
+                        database: configService.get<string>('database.name'),
+                    }),
                 ssl: configService.get<boolean>('database.ssl')
                     ? { rejectUnauthorized: false }
                     : false,
